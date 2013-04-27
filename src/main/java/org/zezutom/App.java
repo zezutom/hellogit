9611bf0da867ad7296573077ef320e396c28f6a3
zezutom <zezulatomas@gmail.com>
2013-04-27 18:03:42 +0200

    feature 001: do some refactoring

--- src/main/java/org/zezutom/App.java
+++ src/main/java/org/zezutom/App.java
@@ -6,8 +6,13 @@ package org.zezutom;
  */
 public class App 
 {
+    public String sayHello() 
+    {
+	return "Hello everyone!";
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello everyone!" );
+        System.out.println(new App().sayHello());
     }
 }
